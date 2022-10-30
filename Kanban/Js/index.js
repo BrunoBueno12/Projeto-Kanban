403a615f2c045696b3171a120228a5d9c965335a
Bruno Bueno <brunobueno2508@gmail.com>
2022-10-30 19:02:54 -0300

    Adicionado a logica do click e arrasta, criado a barra lateral e barra superior, adicionado butoes

---
+++ Kanban/Js/index.js
@@ -0,0 +1,50 @@
+// Logica kanban
+const coluna = document.querySelectorAll(".coluna");
+
+document.addEventListener("dragstart", (e) => {
+  e.target.classList.add("dragging");
+});
+
+document.addEventListener("dragend", (e) => {
+  e.target.classList.remove("dragging");
+});
+
+coluna.forEach((item) => {
+  item.addEventListener("dragover", (e) => {
+    const dragging = document.querySelector(".dragging");
+    const applyAfter = getNewPosition(item, e.clientY);
+
+    if (applyAfter) {
+      applyAfter.insertAdjacentElement("afterend", dragging);
+    } else {
+      item.prepend(dragging);
+    }
+  });
+});
+
+function getNewPosition(coluna, posY) {
+  const cards = coluna.querySelectorAll(".item:not(.dragging)");
+  let result;
+
+  for (let refer_card of cards) {
+    const box = refer_card.getBoundingClientRect();
+    const boxCenterY = box.y + box.height / 2;
+
+    if (posY >= boxCenterY) result = refer_card;
+  }
+
+  return result;
+}
+//---
+
+$('.opition').click(function(){
+    alert ("Editar ou excluir");
+})
+
+$('#adicionar').click(function(){
+    alert ("Adicionar Tarefa");
+})
+
+$('#sair').click(function(){
+    alert ("Logout");
+})
